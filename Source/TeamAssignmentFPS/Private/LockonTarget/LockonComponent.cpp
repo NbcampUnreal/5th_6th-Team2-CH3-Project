9f2862da25b976dc778e94a54a0f1b6a30a03be7
Park Semin <seminpark3317@gmail.com>
2025-10-21 17:41:38 +0900

    mid update with controller constructor

--- Source/TeamAssignmentFPS/Private/LockonTarget/LockonComponent.cpp
+++ Source/TeamAssignmentFPS/Private/LockonTarget/LockonComponent.cpp
@@ -2,33 +2,39 @@
 
 
 #include "LockonTarget/LockonComponent.h"
+#include "Controller/MyPlayerController.h"
 
-// Sets default values for this component's properties
 ULockonComponent::ULockonComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
-// Called when the game starts
+
 void ULockonComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+
 	
 }
 
 
-// Called every frame
 void ULockonComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+
 }
 
+void  ULockonComponent::UpdateCameraBoomLocation()
+{
+}
+
+void  ULockonComponent::UpdateCursorProjection()
+{
+{
+void  ULockonComponent::DetectTarget()
+{
+}
