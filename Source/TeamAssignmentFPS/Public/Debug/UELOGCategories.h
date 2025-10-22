04658c2d45bef1cd47b99797e0be6546860c1095
Park Semin <seminpark3317@gmail.com>
2025-10-22 20:56:24 +0900

    no message

--- Source/TeamAssignmentFPS/Public/Debug/UELOGCategories.h
+++ Source/TeamAssignmentFPS/Public/Debug/UELOGCategories.h
@@ -7,6 +7,6 @@
 //여기에 자신이 쓰는 로그 카테고리 만들어서 본인 작업 장소에서 헤더추가해서 작업하기/ 10/21/2025
 
 // 현 API + Log Declare + 이름
- TEAMASSIGNMENTFPS_API DECLARE_LOG_CATEGORY_EXTERN(UI_Log,Log, All);// UI 관련 디버깅 카테고리
+ TEAMASSIGNMENTFPS_API DECLARE_LOG_CATEGORY_EXTERN(UI_Log, Log, All);// UI 관련 디버깅 카테고리
 
- TEAMASSIGNMENTFPS_API DECLARE_LOG_CATEGORY_EXTERN(IMC_Log,Log, All);// Control 인풋 관련 디버깅
+ TEAMASSIGNMENTFPS_API DECLARE_LOG_CATEGORY_EXTERN(IMC_Log, Log, All);// Control 인풋 관련 디버깅
