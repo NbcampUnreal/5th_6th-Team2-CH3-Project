08bd227b93c377e674c18b38bfa8a8760c107559
Park Semin <seminpark3317@gmail.com>
2025-10-24 12:54:45 +0900

    camera vector setter function added

--- Source/TeamAssignmentFPS/Private/Camera/CameraManager.cpp
+++ Source/TeamAssignmentFPS/Private/Camera/CameraManager.cpp
@@ -5,6 +5,9 @@
 
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Debug/UELOGCategories.h"
+#include "Kismet/GameplayStatics.h"// to get currently using camera as view port
+#include "Kismet/KismetMathLibrary.h"
 
 
 UCameraManagerComp::UCameraManagerComp()
@@ -15,8 +18,15 @@ UCameraManagerComp::UCameraManagerComp()
 	
 	CameraBoom =CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(CameraRoot);
+	CameraBoom->bUsePawnControlRotation(false);// no rotation by controller
+	// rotation lock
+	CameraBoom->bInheritPitch = false;
+	CameraBoom->bInheritYaw = false;
+	CameraBoom->bInheritRoll = false;
+	
 	Camera=CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-	Camera->SetupAttachment(CameraRoot);
+	Camera->SetupAttachment(CameraBoom);
+	Camera->bUsePawnControlRotation(false);// no rotation for camera
 }
 
 
@@ -24,6 +34,8 @@ void UCameraManagerComp::BeginPlay()
 {
 	Super::BeginPlay();
 
+
+	// the way display location is made 
 	
 }
 
@@ -35,8 +47,49 @@ void UCameraManagerComp::TickComponent(float DeltaTime, ELevelTick TickType, FAc
 	// ...
 }
 
-void UCameraManagerComp::UpdateCameraTransform()
+void UCameraManagerComp::ActivateCameraManager()
 {
 	
 }
+
+void UCameraManagerComp::DeactivateCameraManager()
+{
+}
+
+void UCameraManagerComp::AttatchCameraBaseToTarget(USceneComponent* Target)
+{
+}
+
+bool UCameraManagerComp::GetVectorsByCameraAndGravityDirection(const FVector& GravityDirection, FVector& Forward, FVector& Right, FVector& UpVector)
+{
+	if (!GetWorld())
+	{
+		UE_LOG(Camera_Log, Error, TEXT("UCameraManagerComp::GetVectorsByCameraAndGravityDirection->GetWorld Failed"));
+		return false;
+	}
+	
+	APlayerCameraManager* PlayerCameraManager = UGameplayStatics::GetPlayerCameraManager(GetWorld(),0);
+	if (!PlayerCameraManager)
+	{
+		UE_LOG(Camera_Log, Error, TEXT("UCameraManagerComp::GetVectorsByCameraAndGravityDirection->Invalid PlayerCameramanager"));
+		return false;
+	}
+	
+	//rotate the camera rotaion to match with upvector with -gravity direction
+	FRotator CameraRotation = PlayerCameraManager->GetCameraRotation();
+
+	FVector CameraForwardVector=UKismetMathLibrary::GetForwardVector(CameraRotation);
+	FVector TargetUpVector=-GravityDirection.GetSafeNormal();
+
+	//match the camera up vector with TargetUpVector by rotating pitch of the camera rotation
+	FRotator AlignedCameraRotation = UKismetMathLibrary::MakeRotFromXZ(CameraForwardVector, TargetUpVector);
+
+	Forward=UKismetMathLibrary::GetForwardVector(AlignedCameraRotation);
+	Right=UKismetMathLibrary::GetRightVector(AlignedCameraRotation);
+	UpVector=UKismetMathLibrary::GetUpVector(AlignedCameraRotation);
+
+	return true;
+}
+
+
 
--- Source/TeamAssignmentFPS/Public/Camera/CameraManager.h
+++ Source/TeamAssignmentFPS/Public/Camera/CameraManager.h
@@ -20,35 +20,30 @@ public:
 
 protected:
 
-
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CameraManager")
+	bool bIsActive=false;//for default
+	
+	UPROPERTY()
 	UObject* RootOwner;// not the controller, but the parent location of the camera root
-
-
 	
 	//===== Camera ====//
-
-
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")// put it and adjust the transform with offset
 	USceneComponent* CameraRoot;// base root comp
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	USpringArmComponent* CameraBoom;
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
 	UCameraComponent* Camera;
 	
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
-	FVector RootWorldLocation;//where to place the CameraBoom root--> the location will be updated 
+	/*UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	FVector RootWorldLocation;//where to place the CameraBoom root--> the location will be updated */
+	//just use cameraroot->getworldlocation()
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	float LocUpdateSpeed;
 
-
-	
-
-
-
-
-
-	
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
@@ -57,7 +52,23 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-	void UpdateCameraTransform();
+	//=== Activation ===//--> turn on and off of camera manager
+	void ActivateCameraManager();
+	void DeactivateCameraManager();
+	
+	USceneComponent* GetRootComponent() const {return CameraRoot;}// these will be used to change the params like length, view sight, lag speed etc
+	UCameraComponent* GetCameraComponent() const {return Camera;}
+	USpringArmComponent* GetSpringArmComponent() const {return CameraBoom;}
+
+
+	void AttatchCameraBaseToTarget(USceneComponent* Target);
+
+	bool GetVectorsByCameraAndGravityDirection
+	(const FVector& GravityDirection=FVector(0,0,-1), FVector& Forward, FVector& Right, FVector& UpVector);// with default z -1.0
+	// this is for providing character the forward and right vector based on the camera rotation and gravity direction
+	// gravity direction is neeeded to prevent actor tring to move underground when it trys to go forward
+
+	
 
 		
 };
