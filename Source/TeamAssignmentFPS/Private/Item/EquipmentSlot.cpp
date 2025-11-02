c5518e05bf8e97bdb617bd2bdfe42c1edd066ea6
Park Semin <seminpark3317@gmail.com>
2025-11-02 19:13:40 +0900

    Made Seperate code for EquipmentSlot

---
+++ Source/TeamAssignmentFPS/Private/Item/EquipmentSlot.cpp
@@ -0,0 +1,169 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/EquipmentSlot.h"
+#include "Debug/UELOGCategories.h"//debug log
+
+
+// For Slots
+bool FEquipmentSlot::InitializeEquipmentSlot(AActor* Equipment, int32 ID)
+{
+	if(!Equipment)
+	{
+		UE_LOG(Equipment_Manager_Log, Error,
+			TEXT("FEquipmentSlot::InitializeEquipmentSlot-> Invalid EquipmentPtr"));
+		return false;
+	}
+	EquipmentPtr=Equipment;
+	EquipmentID=ID;
+	return true;
+}
+
+//----- ChildClasses---------------------------------------------------------------------------------------------------//
+bool FWeaponSlot::InitializeWeaponData(AActor* Equipment, int32 ID)
+{
+	if (!InitializeEquipmentSlot(Equipment,ID))
+	{
+		return false;
+	}
+	// now can InitializeWeaponData
+
+	//TODO:: add something for weapondata
+
+
+	//after confirmations and initializing is done
+	return true;
+}
+
+bool FItemSlot::IncrementStackCount(bool& BDidReachedMax)
+{
+	if (CurrentStackCount>=MaxStackCount)
+	{
+		UE_LOG(Equipment_Manager_Log,Error,
+			TEXT("FItemSlot::IncrementStackCount-> Not enough space. %d/%d"),CurrentStackCount,MaxStackCount);
+		BDidReachedMax=false;
+		return false;
+	}
+	++CurrentStackCount;
+
+	if (CurrentStackCount>=MaxStackCount)
+	{
+		BDidReachedMax=true;
+		UE_LOG(Equipment_Manager_Log,Warning,
+			TEXT("FItemSlot::IncrementStackCount-> Slot is now full. %d/%d"),CurrentStackCount,MaxStackCount);
+	}
+	
+	BDidReachedMax=false;
+	return true;
+}
+
+bool FItemSlot::DecrementStackCount(bool& DidSlotConsumedAll)
+{
+	
+	if (CurrentStackCount<=0)
+	{
+		UE_LOG(Equipment_Manager_Log,Error,
+			TEXT("FItemSlot::DecrementStackCount-> No more Item, %d/%d"),CurrentStackCount,MaxStackCount);
+		DidSlotConsumedAll=false;// not consumed at all
+		return false;
+	}
+	
+	--CurrentStackCount;//decrement
+
+	if (CurrentStackCount<=0)// after consumed, when no more item to consume
+	{
+		DidSlotConsumedAll=true;
+		UE_LOG(Equipment_Manager_Log,Warning,
+			TEXT("FItemSlot::DecrementStackCount-> No more item left, %d/%d"),CurrentStackCount,MaxStackCount);
+	}
+	
+	DidSlotConsumedAll=false;
+	return true;
+}
+
+bool FItemSlot::InitializeItemData(AActor* Equipment, int32 ID, int32 MaxStack)
+{
+	if (!InitializeEquipmentSlot(Equipment,ID))
+	{
+		return false;
+	}
+	// now check and initialize the item data from here
+
+	if (MaxStack<=0)
+	{
+		UE_LOG(Equipment_Manager_Log,Error,
+			TEXT("FItemSlot::InitializeItemData-> cannot stack item when max stack count is below 0"));
+		return false;
+	}
+	MaxStackCount=MaxStack;
+
+	return true;
+}
+
+// for EquipmentQuickSlot
+bool FEquipmentQuickSlots::AddToSlot(uint8 SlotIndex, TObjectPtr<AActor> Equipment)
+{
+	if(!Equipment)
+	{
+		UE_LOG(Equipment_Manager_Log, Error, 
+		TEXT("FEquipmentQuickSlot::AddToSlot-> Invalid Equipment"));
+		return false;
+	}
+	if (EquipmentQuickSlot.Contains(SlotIndex))
+	{
+		UE_LOG(Equipment_Manager_Log, Error, 
+		TEXT("FEquipmentQuickSlot::AddToSlot-> Slot.%d is already used"),SlotIndex);
+		return false;
+	}
+	if (UsedSlotCount>=SlotMaxCount)// no place to add
+	{
+		UE_LOG(Equipment_Manager_Log, Error,
+			TEXT("FEquipmentQuickSlot::AddToSlot-> Not enough slot to put"));
+		return false;
+	}
+	// checking completed
+	
+	EquipmentQuickSlot.Add(SlotIndex, Equipment);
+
+	if (UsedSlotCount==0)
+	{
+		CurrentSlotIndex=SlotIndex;
+		CurrentSlotEquipment=Equipment;
+	}
+	++UsedSlotCount;
+	return true;
+
+	//Should it set the currentEquipment to newly added one?
+	// ex. Gain weapon-> auto switch to new weapon
+	// or Gain Weapon-> stays in current weapon -> manually switch to new weapon if player request
+}
+
+bool FEquipmentQuickSlots::RemoveFromSlot(uint8 SlotIndex)
+{
+	if (!EquipmentQuickSlot.Contains(SlotIndex))
+	{
+		UE_LOG(Equipment_Manager_Log, Error, TEXT("FEquipmentQuickSlot::RemoveFromSlot-> ID:%d not found in the slot"),SlotIndex);
+		return false;
+	}
+
+	EquipmentQuickSlot.Remove(SlotIndex);
+	--UsedSlotCount;
+
+	if (CurrentEquipmentID!=SlotIndex)// when removed equipment was not from current slot
+	{
+		return true;// just return
+	}
+
+	// removed one is current slot. find new slot to replace current slot
+
+	if (UsedSlotCount<=0)// empty slots. no equipments
+	{
+		CurrentSlotIndex=INDEX_NONE;
+		CurrentSlotEquipment=nullptr;
+		return true;
+		// should it set to the first one? or last one?
+	}
+	// when there is a replacement slot
+
+	return false;// temp
+}
---
+++ Source/TeamAssignmentFPS/Public/Item/EquipmentSlot.h
@@ -0,0 +1,98 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+USTRUCT(BlueprintType)
+struct FEquipmentSlot// one slot
+{
+	GENERATED_BODY()
+	
+	UPROPERTY()
+	TObjectPtr<AActor> EquipmentPtr=nullptr;
+
+private:
+	UPROPERTY()
+	int32 EquipmentID=INDEX_NONE;
+
+public:
+	bool InitializeEquipmentSlot(AActor* Equipment,int32 ID);
+
+	int32 GetEquipmentID()const {return EquipmentID;}
+	AActor* GetEquipmentPtr() const {return EquipmentPtr;}
+};
+
+// make Child class of slots (Weapon, Items)
+
+USTRUCT(BlueprintType)
+struct FWeaponSlot:public FEquipmentSlot// one slot
+{
+	GENERATED_BODY()
+
+	/*
+	 TODO: put required infos in here for weapon
+
+	 */
+	
+protected:
+	bool InitializeWeaponData(AActor* Equipment,int32 ID);
+};
+
+USTRUCT(BlueprintType)
+struct FItemSlot:public FEquipmentSlot// one slot
+{
+	GENERATED_BODY()
+	
+private:
+	int32 MaxStackCount=0;
+	int32 CurrentStackCount=INDEX_NONE;
+
+public:
+	int32 GetCurrentStackCount()const {return MaxStackCount;}
+	int32 GetMaxStackCount()const {return CurrentStackCount;}
+
+	bool IncrementStackCount(bool& BDidReachedMax);// only increment and decrement with 1 cause the leftover case happens
+	// bool param for checking if the slot is full or not after increment
+	bool DecrementStackCount(bool& DidSlotConsumedAll);
+	// param for checking if the slot used all of the stacks and need to removed from quick slots
+
+	
+	bool InitializeItemData(AActor* Equipment,int32 ID, int32 MaxStack);
+};
+
+
+
+
+// slot struct to manage equipments
+USTRUCT(BlueprintType)
+struct FEquipmentQuickSlots//multiple slots
+{
+	GENERATED_BODY()
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Equipment Slot")
+	TMap<uint8/*slot Number*/,TObjectPtr<AActor>/*Equipment*/> EquipmentQuickSlot;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Equipment Slot")
+	int32 SlotMaxCount;
+	
+
+private:
+	
+	int32 CurrentEquipmentID=INDEX_NONE;//default
+	int32 UsedSlotCount=0;
+	uint8 CurrentSlotIndex=1;// the main
+	
+	UPROPERTY(VisibleAnywhere, Category = "Equipment Slot")
+	AActor* CurrentSlotEquipment=nullptr;// not currently equipt by player but equipment of current slot index
+	
+public:
+	AActor* GetEquipmentFromCurrentSlot() const { return CurrentSlotEquipment;}
+	uint8 GetCurrentSlotIndex() const { return CurrentSlotIndex; }
+	int32 GetCurrentEquipmentID() const { return CurrentEquipmentID;}
+
+	bool AddToSlot(uint8 SlotIndex, TObjectPtr<AActor> Equipment);
+	bool RemoveFromSlot(uint8 SlotIndex);
+
+};
