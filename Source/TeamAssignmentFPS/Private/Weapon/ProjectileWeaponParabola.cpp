9dec185e81a9b26d9c2add088e219b4d130e435e
Park Semin <seminpark3317@gmail.com>
2025-11-10 19:31:42 +0900

    parabola weapon update

---
+++ Source/TeamAssignmentFPS/Private/Weapon/ProjectileWeaponParabola.cpp
@@ -0,0 +1,94 @@
+﻿#include "Weapon/ParabolaWeapon.h"
+#include "Weapon/ProjectilePathDrawer.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#include "TimerManager.h"
+
+AParabolaWeapon::AParabolaWeapon()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+}
+
+void AParabolaWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bIsCharging)
+	{
+		CurrentChargeTime += DeltaTime;
+		float ChargeRatio = FMath::Clamp(CurrentChargeTime / MaxChargeTime, 0.f, 1.f);
+		DrawParabolaPath(ChargeRatio);
+	}
+}
+
+
+void AParabolaWeapon::OnInputTap_Implementation()
+{
+	// Quick tap -> fire fast projectile
+	bIsCharging = false;
+	CurrentChargeTime = MinChargeTime;
+	LaunchParabolaProjectile();
+}
+
+void AParabolaWeapon::OnInputHoldStart_Implementation()
+{
+	// Begin holding
+	bIsCharging = true;
+	CurrentChargeTime = 0.f;
+}
+
+void AParabolaWeapon::OnInputHoldUpdate_Implementation(float InputValue)
+{
+	CurrentChargeTime = FMath::Clamp(CurrentChargeTime + InputValue, 0.f, MaxChargeTime);
+}
+
+void AParabolaWeapon::OnInputRelease_Implementation()
+{
+	// Release -> fire with Stacked charge value
+	if (bIsCharging)
+	{
+		bIsCharging = false;
+		LaunchParabolaProjectile();
+		CurrentChargeTime = 0.f;
+	}
+}
+
+void AParabolaWeapon::LaunchParabolaProjectile()
+{
+	if (!ProjectileClass || !Muzzle)
+		return;
+
+	// Compute throw distance based on charge
+	float ChargeRatio = FMath::Clamp(CurrentChargeTime / MaxChargeTime, 0.f, 1.f);
+	float ThrowDistance = FMath::Lerp(MinThrowDistance, MaxThrowDistance, ChargeRatio);
+	float ApexHeight = FMath::Lerp(MinParabolaHeight, MaxParabolaHeight, ChargeRatio);
+
+	FVector SpawnLocation = Muzzle->GetComponentLocation();
+	FRotator SpawnRotation = Muzzle->GetComponentRotation();
+	FVector TargetLocation = SpawnLocation + SpawnRotation.Vector() * ThrowDistance;
+
+	// Spawn the projectile from pool
+	AParabola_ProjectileBase* Projectile = SpawnProjectile<AParabola_ProjectileBase>(true, SpawnLocation, SpawnRotation);// use template to spawn different one
+	if (Projectile)
+	{
+		Projectile->SetStartAndEndLocation(SpawnLocation,TargetLocation);
+		Projectile->SetMaxHeightForParabola(ApexHeight);
+
+		Projectile->SetDamageInfo(DamageInfo);
+	}
+}
+
+void AParabolaWeapon::DrawParabolaPath(float ChargeRatio)//temp, debug
+{
+	if (!Muzzle)
+		return;
+
+	float ThrowDistance = FMath::Lerp(MinThrowDistance, MaxThrowDistance, ChargeRatio);
+	float ApexHeight = FMath::Lerp(MinParabolaHeight, MaxParabolaHeight, ChargeRatio);
+
+	FVector StartLocation = Muzzle->GetComponentLocation();
+	FVector EndLocation = StartLocation + Muzzle->GetComponentRotation().Vector() * ThrowDistance;
+
+	UProjectilePathDrawer::DrawLerpedArc(GetWorld(), StartLocation, EndLocation, 1.f, StartLocation.Z, StartLocation.Z + ApexHeight, PathSegments, PathColor, 0.f);
+}
---
+++ Source/TeamAssignmentFPS/Public/Weapon/ParabolaWeapon.h
@@ -0,0 +1,61 @@
+﻿// ProjectileWeaponParabola.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/ProjectileWeaponBase.h"
+#include "Parabola_ProjectileBase.h"
+#include "ParabolaWeapon.generated.h"
+
+UCLASS()
+class TEAMASSIGNMENTFPS_API AParabolaWeapon : public AProjectileWeaponBase
+{
+	GENERATED_BODY()
+
+public:
+	AParabolaWeapon();
+
+protected:
+	/** Charging and input */
+	bool bIsCharging = false;
+	float CurrentChargeTime = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Parabola | Charging")
+	float MaxChargeTime = 2.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Parabola | Charging")
+	float MinChargeTime = 0.2f;
+
+	/** Parabola settings */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Parabola | Settings")
+	float MinThrowDistance = 500.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Parabola | Settings")
+	float MaxThrowDistance = 2000.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Parabola | Settings")
+	float MinParabolaHeight = 100.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Parabola | Settings")
+	float MaxParabolaHeight = 600.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Parabola | Settings")
+	int32 PathSegments = 24;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Parabola | Settings")
+	FColor PathColor = FColor::Green;
+
+protected:
+	virtual void Tick(float DeltaTime) override;
+
+	/** InputReactionInterface */
+	virtual void OnInputTap_Implementation() override;
+	virtual void OnInputHoldStart_Implementation() override;
+	virtual void OnInputHoldUpdate_Implementation(float InputValue) override;
+	virtual void OnInputRelease_Implementation() override;
+
+	/** Fire the parabola projectile */
+	void LaunchParabolaProjectile();
+
+	/** Draw debug path */
+	void DrawParabolaPath(float ChargeRatio);
+};
