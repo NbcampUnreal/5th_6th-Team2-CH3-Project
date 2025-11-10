6a5cf81de1e9d5e0ce8a8a93b77ef9566e5d940e
Park Semin <seminpark3317@gmail.com>
2025-11-11 06:43:56 +0900

    pistol added

---
+++ Source/TeamAssignmentFPS/Private/Weapon/PistolWeapon.cpp
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/PistolWeapon.h"
+
+
+// Sets default values
+APistolWeapon::APistolWeapon()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Default settings for pistol
+	MaxAmmoCount = 12;
+	CurrentAmmoCount = MaxAmmoCount;
+	SpawnInterval = 0.2f; 
+	ReloadTime = 1.5f;
+}
+
+void APistolWeapon::AutoFire()
+{
+	if (bIsReloading || CurrentAmmoCount <= 0)
+	{
+		// stop auto-fire if no ammo or reloading
+		OnInputRelease_Implementation();
+
+		if (CurrentAmmoCount <= 0)
+		{
+			ReloadWeapon();
+		}
+		return;
+	}
+
+	FireWeapon();
+}
+
+void APistolWeapon::OnInputTap_Implementation()
+{
+	FireWeapon();
+}
+
+void APistolWeapon::OnInputHoldStart_Implementation()
+{
+	if (!bIsAutoFiring)
+	{
+		FireWeapon(); // first shot immediately
+		bIsAutoFiring = true;
+		GetWorldTimerManager().SetTimer(
+			AutoFireTimerHandle,
+			this,
+			&APistolWeapon::AutoFire,
+			SpawnInterval,
+			true
+		);
+	}
+}
+
+void APistolWeapon::OnInputRelease_Implementation()
+{
+	if (bIsAutoFiring)
+	{
+		bIsAutoFiring = false;
+		GetWorldTimerManager().ClearTimer(AutoFireTimerHandle);
+	}
+}
+
---
+++ Source/TeamAssignmentFPS/Public/Weapon/PistolWeapon.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectileWeaponBase.h"
+#include "PistolWeapon.generated.h"
+
+UCLASS()
+class TEAMASSIGNMENTFPS_API APistolWeapon : public AProjectileWeaponBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APistolWeapon();
+
+protected:
+	
+	FTimerHandle AutoFireTimerHandle;
+	bool bIsAutoFiring;
+	void AutoFire();
+	
+public:
+	
+	virtual void OnInputTap_Implementation() override;
+	virtual void OnInputHoldStart_Implementation() override;
+	virtual void OnInputRelease_Implementation() override;
+};
