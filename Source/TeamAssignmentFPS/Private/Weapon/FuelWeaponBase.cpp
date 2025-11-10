c718ef625ac243339c034cb9883ae5cfec4cc958
Park Semin <seminpark3317@gmail.com>
2025-11-10 16:39:33 +0900

    fuel weapon update

--- Source/TeamAssignmentFPS/Private/Weapon/FuelWeaponBase.cpp
+++ Source/TeamAssignmentFPS/Private/Weapon/FuelWeaponBase.cpp
@@ -2,3 +2,68 @@
 
 
 #include "Weapon/FuelWeaponBase.h"
+
+
+// Sets default values
+AFuelWeaponBase::AFuelWeaponBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AFuelWeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AFuelWeaponBase::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
+
+void AFuelWeaponBase::OnReloadInputPressed_Implementation()
+{
+	IWeaponInterface::OnReloadInputPressed_Implementation();
+}
+
+void AFuelWeaponBase::OnInputHoldStart_Implementation()
+{
+	IInputReactionInterface::OnInputHoldStart_Implementation();
+}
+
+void AFuelWeaponBase::OnInputHoldUpdate_Implementation(float InputValue)
+{
+	IInputReactionInterface::OnInputHoldUpdate_Implementation(InputValue);
+}
+
+void AFuelWeaponBase::OnInputRelease_Implementation()
+{
+	IInputReactionInterface::OnInputRelease_Implementation();
+}
+
+void AFuelWeaponBase::OnEquipped_Implementation()
+{
+	IEquipmentInterface::OnEquipped_Implementation();
+}
+
+void AFuelWeaponBase::OnUnequipped_Implementation()
+{
+	IEquipmentInterface::OnUnequipped_Implementation();
+}
+
+// Called every frame
+void AFuelWeaponBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AFuelWeaponBase::FireWeapon()
+{
+}
+
+void AFuelWeaponBase::StopFiringWeapon()
+{
+}
+
