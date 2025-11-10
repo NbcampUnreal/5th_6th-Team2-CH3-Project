a2a0de0b97ed5b6c237f5a430d0cc93f12776a61
Park Semin <seminpark3317@gmail.com>
2025-11-11 06:51:45 +0900

    automated weapon added

---
+++ Source/TeamAssignmentFPS/Private/Weapon/AutoGunWeapon.cpp
@@ -0,0 +1,81 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/AutoGunWeapon.h"
+
+
+// Sets default values
+AAutoGunWeapon::AAutoGunWeapon()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpawnInterval = 0.15f; // fire rate for auto-fire
+	MaxAmmoCount = 30;
+	CurrentAmmoCount = MaxAmmoCount;
+	ReloadTime = 2.f;
+
+	// Burst settings
+	MaxBurstShots = 3;
+	BurstShotsFired = 0;
+	bIsAutoFiring = false;
+}
+
+void AAutoGunWeapon::AutoFire()
+{
+	if (!bIsAutoFiring || CurrentAmmoCount <= 0)
+	{
+		bIsAutoFiring = false;
+		GetWorldTimerManager().ClearTimer(AutoFireTimerHandle);
+		return;
+	}
+
+	FireWeapon();
+	++BurstShotsFired;
+
+	// Stop burst after max shots for tap
+	if (bIsTapBurst && BurstShotsFired >= MaxBurstShots)
+	{
+		bIsAutoFiring = false;
+		GetWorldTimerManager().ClearTimer(AutoFireTimerHandle);
+	}
+}
+
+void AAutoGunWeapon::OnInputTap_Implementation()
+{
+	if (bIsReloading || CurrentAmmoCount <= 0) return;
+
+	bIsTapBurst = true;
+	bIsAutoFiring = true;
+	BurstShotsFired = 0;
+
+	GetWorldTimerManager().SetTimer(
+		AutoFireTimerHandle,
+		this,
+		&AAutoGunWeapon::AutoFire,
+		SpawnInterval,
+		true
+	);
+}
+
+void AAutoGunWeapon::OnInputHoldStart_Implementation()
+{
+	if (bIsReloading || CurrentAmmoCount <= 0) return;
+
+	bIsTapBurst = false; // hold mode
+	bIsAutoFiring = true;
+
+	GetWorldTimerManager().SetTimer(
+		AutoFireTimerHandle,
+		this,
+		&AAutoGunWeapon::AutoFire,
+		SpawnInterval,
+		true
+	);
+}
+
+void AAutoGunWeapon::OnInputRelease_Implementation()
+{
+	bIsAutoFiring = false;
+	GetWorldTimerManager().ClearTimer(AutoFireTimerHandle);
+}
---
+++ Source/TeamAssignmentFPS/Public/Weapon/AutoGunWeapon.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectileWeaponBase.h"
+#include "AutoGunWeapon.generated.h"
+
+UCLASS()
+class TEAMASSIGNMENTFPS_API AAutoGunWeapon : public AProjectileWeaponBase
+{
+	GENERATED_BODY()
+
+public:
+	AAutoGunWeapon();
+
+protected:
+	// Auto-fire timer
+	FTimerHandle AutoFireTimerHandle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Weapon | AutoGun")
+	int32 MaxBurstShots = 3;
+	
+	int32 BurstShotsFired = 0;// counting
+
+	bool bIsTapBurst=false;
+	bool bIsAutoFiring=false;
+
+	// Called repeatedly while holding
+	void AutoFire();
+
+public:
+	// Input overrides
+	virtual void OnInputTap_Implementation() override;
+	virtual void OnInputHoldStart_Implementation() override;
+	virtual void OnInputRelease_Implementation() override;
+};
