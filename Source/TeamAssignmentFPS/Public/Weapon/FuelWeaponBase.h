897ecb31da96cc6f4c85beb655d21918826beb20
Park Semin <seminpark3317@gmail.com>
2025-11-10 16:39:19 +0900

    fuel weapon update

---
+++ Source/TeamAssignmentFPS/Public/Weapon/FuelWeaponBase.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "FuelWeaponBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAMASSIGNMENTFPS_API UFuelWeaponBase : public UObject
+{
+	GENERATED_BODY()
+};
